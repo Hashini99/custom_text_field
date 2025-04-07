6963d93e4c73f91cb715a3e77469adacf0b09822
Hashini Rashmika <hashini.rashmika@itechro.com>
2025-04-07 11:58:31 +0530

    add example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,61 +1,42 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:custom_text_field_plugin/custom_text_field_plugin.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _customTextFieldPlugin = CustomTextFieldPlugin();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _customTextFieldPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final TextEditingController textController = TextEditingController();
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
+        appBar: AppBar(title: const Text('Using Custom Plugin')),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CustomTextField(
+                  controller: textController,
+                  hint: "Enter Name",
+                  labelText: "Name",
+                  prefixIcon: Icon(Icons.person, color: Colors.black),
+                  borderColor: Colors.green,
+                  fillColor: Colors.white,
+                  borderWidth: .9,
+                  obscureText: true,
+                  onChanged: (value) {
+                    print("Text: $value");
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
